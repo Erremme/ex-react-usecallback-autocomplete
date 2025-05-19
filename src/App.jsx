61bbdc6ea0a194468f6059e740b6c7e2feef29c5
Erremme <Erremme2.0@gmail.com>
2025-05-19 14:56:17 +0200

    feat: implementd fetchProductDetails

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import debounce from "lodash/debounce"
 function Card({brand , description, name , price , image} ) {
   return(
     <div className="card-container">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
+      
+      <img src={image}  alt={name} />
       <p><strong>Brand :</strong> {brand}</p>
       <p><strong>Descrizione prodotto:</strong> {description}</p>
-      <img src={image}  alt={name} />
-      <p><strong>Prezzo :</strong> {price}</p>
+      <p><strong>Prezzo :</strong> {price} $</p>
     </div>
   )
 }
@@ -18,6 +19,7 @@ function Card({brand , description, name , price , image} ) {
 export default function App () {
   const [query , setQuery] = useState("")
   const [products , setProducts] = useState([])
+  const [selectedProduct , setSelectedProduct] = useState(null)
 
 
    const handleSearch = (query) => {
@@ -39,43 +41,55 @@ export default function App () {
      []
    )
    
-    console.log(products)
    useEffect(() => {
     debouncedHandleSearch(query)
    },[query])
 
-   
+   const fetchProductDetails = (id) => {
+     fetch(`http://localhost:5000/products/${id}`)
+     .then((res) => res.json())
+     .then((data) => {
+      setSelectedProduct(data)
+      setQuery("")
+      setProducts([])
+     })
+     .catch((error) => console.error(error))
+   }
 
 
-  console.log(query)
   return(
     <div className="container">
       <div className="search-container">
         <label htmlFor="text"> Cerca Prodotto</label>
         <input type="text" 
+        placeholder="Cerca un prodotto..."
          value={query}
          onChange={(e) => setQuery(e.target.value)}
         />
       </div>
-      {products && 
-        <div>
-          <h1>Prodotti trovati</h1>
+      {products.length  > 0 && 
+        <div className="result-container">
+          
           <ul>
             {products.map((product) => {
               return(
-              <li key={product.id}>
-                <Card 
-                name={product.name}
-                brand={product.brand}
-                description={product.description}
-                price={product.price}
-                image={product.image}
-                />
+              <li key={product.id} onClick={() => fetchProductDetails(product.id)}>
+                <p>{product.name}</p>
               </li>
             )
             })}
           </ul>
         </div>
+      }
+
+      {selectedProduct && 
+       <Card 
+       name={ selectedProduct.name}
+       description={selectedProduct.description}
+       image={selectedProduct.image}
+       price={selectedProduct.price}
+       brand={selectedProduct.brand}
+       />
       }
     </div>
   )
